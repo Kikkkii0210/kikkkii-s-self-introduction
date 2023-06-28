2f5e681b36a00d58e876f1c4c3507c27bb88ef86
Kikkkii0210 <137807515+Kikkkii0210@users.noreply.github.com>
2023-06-29 05:16:11 +0800

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-window.scroll(0, 500);
+angular.module('tabApp', [])
+  .controller('TabController', ['$scope', function($scope) {
+    $scope.tab = 1;
+
+    $scope.setTab = function(newTab){
+      $scope.tab = newTab;
+    };
+
+    $scope.isSet = function(tabNum){
+      return $scope.tab === tabNum;
+    };
+}]);
+
+let btn=document.querySelector("#show1");
+let infoModal=document.querySelector("#infoModal");
+let close=document.querySelector("#close1");
+btn.addEventListener("click", function(){
+  infoModal.showModal();
+})
+close.addEventListener("click", function(){
+  infoModal.close();
+})
+
